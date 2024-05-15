c3d89f3be7c87980097964e9975321d2e9546a3d
wj <wangjian@sft360.com>
2024-05-15 11:29:58 +0800

    提交人：wj 提交内容： 1.完善接口调用工具类

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # 一个包含很多东西的demo
 ## 1.国际化i18n
     1.三层实现 静态文件/常量+缓存+数据库
-## 2.产品化参数传递，分发实现思路baseContext
-    1.分发
+## 2.产品化 DDD强化版的架构思路
+    1.参考HandlerAdapter
+    2.baseContext 
 ## 3.mybatis自动赋值初始字段插件
     1.基本数据填充
     2.分页插件 注解@Page 分页参数可以放到baseContext
@@ -16,52 +17,48 @@
     4.初始化线程池
     4.异步工具类completeFutrue,参考JD AsyncTools
     5.邮件工具类，短信工具类
-## 6.API模块
+## 6.API模块 todo
     1.t_api 记录所有的请求url，description，specification，publisher，environment，code(1.通过代码获取地址,代码放到常量或者枚举，2.支持前端配置，保存到数据库)
-    2.t_api_subscription 记录订阅人，订阅系统，通行证key，status(待审批,已订阅)
+    2.t_api_subscription 记录订阅人，订阅系统，通行证token，status(待审批,已订阅)
     3.t_api_record 记录所有的调用记录（切面），入参出参，支持重发
-## 7.文件管理模块
+## 7.文件管理模块 todo
     1.导入导出
     2.文件上传下载
     3.json转excel，excel转json，json转对象...
     4.ExcelUtils实现简单excel的导出
-## 8.字典模块
+## 8.字典模块 todo
     1.t_dict(code,label)
     2.t_dict_language关联国际化(t_dict_id,t_language_id)
-## 9.配置管理 (通过页面配置实现功能的部分定制化)
+## 9.配置管理 (通过页面配置实现功能的部分定制化) todo
     1.t_config(company，configCode，description,configInfo)
-## 10.DDD强化版的架构思路
-    1.参考HandlerAdapter
-## 11.权限认证
+## 10.权限认证 todo
     1.登录认证
     2.访问拦截
     3.接口权限控制 注解或者配置 实现
     4.数据权限配置 行权限，列权限
     5.角色权限（用户管理，菜单管理，组织管理，角色管理）
-## 12.微服务SpringCloudAlibaba(Nacos,Feign)
+## 11.微服务SpringCloudAlibaba(Nacos,Feign) todo
     1.服务调用
     2.注册发现
     3.负载均衡
     4.链路追踪
     5.服务降级
-## 13.redis
+## 12.redis
     1.RedisUtils工具类简化，包含批量，上锁，解锁
-## 14.mq
-## 15.websocket/SSE
-## 16.swagger
-## 17.多数据源druid
-## 18.定时任务xxl-job
-## 19.流程管理 组件化 可配置
+## 13.mq todo
+## 14.websocket/SSE todo
+## 15.swagger todo
+## 16.多数据源druid todo
+## 17.定时任务xxl-job todo
+## 18.流程管理 组件化 可配置 todo
     1.t_process(node,description,提交人，当前审批人，状态)
-## 20.支付模块
-## 21.节假日管理
+## 19.支付模块 todo
+## 20.节假日管理 todo
     1.自动获取国务院信息，解析存表，生成日历，包含节假日信息
-## 22.Timer定时器
+## 21.Timer定时器
     1.用于实现XX时间后执行任务
-    2.能共用一个定时器吗？
-    3.放到一个容器统一管理任务TimerTask
-    4.反射执行 参数放到一起，封装起来
-
-
-## todo
-    1.日志文件自动删除，超过多少天或者多大
+    2.参照ScheduledExecutorUtils
+## 22.规则引擎 todo
+## 23.排产引擎 todo
+## 24.el表达式 解析 todo
+## 25.日志文件自动删除，超过多少天或者多大 todo
--- src/main/java/com/wj/demo/common/utils/HttpUtils.java
+++ src/main/java/com/wj/demo/common/utils/HttpUtils.java
@@ -3,6 +3,8 @@ package com.wj.demo.common.utils;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 /**
  * @author wj
  * @version 1.0
@@ -11,28 +13,51 @@ import org.springframework.web.client.RestTemplate;
  */
 public class HttpUtils {
 
-    private static RestTemplate template = null;
+    private static RestTemplate TEMPLATE = null;
 
-    public static RestTemplate getRestTemplate() {
-        if (template == null) {
-            SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-            requestFactory.setConnectTimeout(0);
-            requestFactory.setReadTimeout(0);
-            template = new RestTemplate(requestFactory);
-        }
-        return template;
+    static {
+        //初始化默认RestTemplate
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(0);
+        requestFactory.setReadTimeout(0);
+        TEMPLATE = new RestTemplate(requestFactory);
     }
 
     /**
      * post请求 todo 加个切面 记录入参出参，url
-     * @param url
-     * @param param
-     * @param clazz
-     * @return
-     * @param <T>
+     *
+     * @param url    地址
+     * @param params 参数
+     * @param clazz  返回类型
+     * @param <T>    返回泛型
+     * @return 返回值
      */
-    public <T> T post(String url, String param, Class<T> clazz) {
-        return getRestTemplate().postForObject(url, param, clazz);
+    public <T> T post(String url, Object params, Class<T> clazz) {
+        return TEMPLATE.postForObject(url, params, clazz);
     }
 
+    /**
+     * get请求 无参数
+     *
+     * @param url   地址
+     * @param clazz 返回类型
+     * @param <T>   返回泛型
+     * @return 返回值
+     */
+    public <T> T post(String url, Class<T> clazz) {
+        return TEMPLATE.getForObject(url, clazz);
+    }
+
+    /**
+     * get请求 带参数
+     *
+     * @param url    地址
+     * @param clazz  返回类型
+     * @param params 参数
+     * @param <T>    返回泛型
+     * @return 返回值
+     */
+    public <T> T post(String url, Class<T> clazz, Map<String, ?> params) {
+        return TEMPLATE.getForObject(url, clazz, params);
+    }
 }
