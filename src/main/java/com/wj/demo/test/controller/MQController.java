77f3758333c4481b1fd63d72b1f32971bcf3c7eb
wj <wangjian@sft360.com>
2024-05-15 16:57:34 +0800

    提交人：wj 提交内容： 1.集成mq

---
+++ src/main/java/com/wj/demo/test/controller/MQController.java
@@ -0,0 +1,41 @@
+package com.wj.demo.test.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import jakarta.annotation.Resource;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wj
+ * @version 1.0
+ * @Desc
+ * @date 2024/5/15 16:27
+ */
+@RestController
+@RequestMapping("mq")
+public class MQController {
+
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+
+    @PostMapping("send")
+    public void send(){
+        for (int i = 0; i < 10; i++) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("index",i);
+            rabbitTemplate.convertAndSend(jsonObject);
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+}
