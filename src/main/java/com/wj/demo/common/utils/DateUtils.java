96d5f541640aace65ce955254dda3940ff03540a
wj <wangjian@sft360.com>
2024-06-26 09:52:02 +0800

    提交人：wj 提交内容： 1.日期工具类 2.类型转json工具类 不保留内层对象 3.流程父类VO

--- src/main/java/com/wj/demo/common/utils/DateUtils.java
+++ src/main/java/com/wj/demo/common/utils/DateUtils.java
@@ -1,5 +1,6 @@
 package com.wj.demo.common.utils;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -10,8 +11,26 @@ import java.util.Date;
  */
 public class DateUtils {
 
-    public static Date newDate(){
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
+    public static final String YYYY_MM_DD_HH_MM_DD = "yyyy-MM-dd HH:mm:ss";
+
+    private static SimpleDateFormat getFormatter(String pattern) {
+        if (pattern == null) {
+            return new SimpleDateFormat(YYYY_MM_DD_HH_MM_DD);
+        }
+        return new SimpleDateFormat(pattern);
+    }
+
+    public static Date newDate() {
         return new Date();
     }
 
+    public static String parse(Date date) {
+        return getFormatter(YYYY_MM_DD_HH_MM_DD).format(date);
+    }
+
+    public static String parse(Date date, String pattern) {
+        return getFormatter(pattern).format(date);
+    }
 }
---
+++ src/test/java/com/wj/demo/test/MyTest.java
@@ -0,0 +1,27 @@
+package com.wj.demo.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author wj
+ * @version 1.0
+ * @Desc
+ * @date 2024/6/6 14:20
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        int price = 20;
+        int profit=3;
+        int personCount = 0;
+
+        //每件商品20，利润x，每有一个人拉4人成团这个人就可以全额返现,拉多少个人可以盈利 总利润？
+
+
+    }
+
+    @Test
+    public void getBean() {
+
+    }
+}
