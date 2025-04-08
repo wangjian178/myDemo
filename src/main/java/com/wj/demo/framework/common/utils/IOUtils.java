daed3d63e7982e826c27a018990fcc3136659b8b
wj <1783217658@qq.com>
2025-04-08 15:45:17 +0800

    提交人：wj 提交内容： 1.完善IO工具类

--- src/main/java/com/wj/demo/framework/common/utils/IOUtils.java
+++ src/main/java/com/wj/demo/framework/common/utils/IOUtils.java
@@ -1,5 +1,8 @@
 package com.wj.demo.framework.common.utils;
 
+import com.wj.demo.framework.exception.exception.BaseException;
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -7,122 +10,125 @@ import java.io.IOException;
 
 /**
  * @ClassName IOUtils
- * @Description  io流工具类
+ * @Description io流工具类
  * @Author W.Jian
  * @Date 2021/7/6 9:45
  **/
+@Slf4j
 public class IOUtils {
-
-    public static void main(String[] args) {
-        writeToFile("lalala","C:\\Users\\W.Jian\\Desktop\\新建文本文档.txt");
-    }
 
     /**
      * 从文件中读取文本
-     * @param filePath
-     * @return
+     *
+     * @param filePath 路径
+     * @return 文本
      */
-    public static String readFromFile(String filePath){
-        if(filePath == null || filePath.equals("")){
-            System.out.println("路径为空！");
-            return "路径不能为空！";
+    public static String readFromFile(String filePath) {
+        if (filePath == null || filePath.isEmpty()) {
+            throw new BaseException("文件路径不能为空！");
         }
+
         File file = new File(filePath);
-        if(!file.exists()){
-            System.out.println("目标文件不存在！");
-            return "目标文件不存在！";
+        if (!file.exists()) {
+            throw new BaseException("目标文件不存在！");
+        }
+        if (!file.canRead()) {
+            throw new BaseException("目标文件不可读！");
         }
         StringBuilder builder = new StringBuilder();
-        try {
-            FileReader fr = new FileReader(file);
+        try (FileReader fr = new FileReader(file)) {
             //定义char数组
             char[] data = new char[23];
 
             int length = 0;
             //循环读取文件中的数据
-            while((length = fr.read(data))>0){
+            while ((length = fr.read(data)) > 0) {
                 //根据读取文件的内容创建String 对象
-                String str = new String(data,0,length);
+                String str = new String(data, 0, length);
                 //输出读取内容
                 builder.append(str);
             }
-            //关闭流
-            fr.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("文件读取失败", e);
+            throw new BaseException("文件读取失败");
         }
+        log.info("文件读取成功。。。{}", builder);
         return builder.toString();
     }
 
     /**
      * 将文本写入文件
-     * @param filePath
-     * @return
+     *
+     * @param content  文本
+     * @param filePath 路径
      */
-    public static String writeToFile(String content,String filePath){
-        if(content == null){
-            return "内容为空！";
+    public static void writeToFile(String content, String filePath) {
+        if (content == null) {
+            return;
         }
-        if(filePath == null || filePath.equals("")){
-            return "路径不能为空！";
+        if (filePath == null || filePath.isEmpty()) {
+            return;
         }
         //创建指定文件
         File file = new File(filePath);
-        try {
-            if(! file.exists()){
-                //如果指定文件不存在，新建文件
-                System.out.println("文件不存在，新建文件。。。");
-                file.createNewFile();
+        if (!file.exists()) {
+            //如果指定文件不存在，新建文件
+            try {
+                boolean createStatus = file.createNewFile();
+                log.info("文件不存在，新建文件。。。{}  {}", filePath, createStatus);
+            } catch (IOException e) {
+                log.error("新建文件失败", e);
+                throw new BaseException("新建文件失败");
             }
-            //创建FileWriter对象
-            FileWriter fw = new FileWriter(file);
-            //向文件写入数据
-            fw.write(content.toCharArray());
-			//关闭流
-            fw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
-
-        return "";
+        if (!file.canWrite()) {
+            throw new BaseException("文件不可写入");
+        }
+        //创建FileWriter对象
+        try (FileWriter fw = new FileWriter(file)) {
+            //向文件写入数据
+            fw.write(content);
+        } catch (IOException e) {
+            log.error("文件写入失败", e);
+        }
+        log.info("文件写入成功。。。{}", filePath);
     }
 
     /**
      * 从一个文件读取到另一个文件
-     * @param source
-     * @param target
-     * @return
+     *
+     * @param source 路径
+     * @param target 目标
      */
-    public static String readFromSourceAndWriteToTarget(String source,String target){
-        if(StringUtils.isEmpty(source) || StringUtils.isEmpty(target)){
-            return "路径不能为空！";
+    public static void readFromSourceAndWriteToTarget(String source, String target) {
+        if (StringUtils.isEmpty(source)) {
+            throw new BaseException("来源路径不能为空！");
+        }
+        if (StringUtils.isEmpty(target)) {
+            throw new BaseException("目标路径不能为空！");
         }
         //创建指定文件
         File file = new File(source);
-        if(! file.exists()){
+        if (!file.exists()) {
             //如果指定文件不存在，新建文件
-            System.out.println("来源文件不存在");
-            return "来源文件不存在！";
+            throw new BaseException("来源文件不存在！");
         }
 
         try {
             File targetFile = new File(target);
-            if(! targetFile.exists()){
-                System.out.println("目标文件不存在，正在创建");
-                targetFile.createNewFile();
+            if (!targetFile.exists()) {
+                boolean createStatus = targetFile.createNewFile();
+                log.info("目标文件不存在，正在创建。。。{}", createStatus);
             }
             //创建FileWriter对象
-            FileWriter fw = new FileWriter(file);
+            FileWriter fw = new FileWriter(targetFile);
             //向文件写入数据
-            //fw.write(content.toCharArray());
+            fw.write(readFromFile(source));
             //关闭流
             fw.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("文件读取写入失败", e);
         }
-
-
-        return "";
+        log.info("文件读取写入成功。。。{}", target);
     }
 }
